7390d6a509a8a7dd50bfd604f76c42e900f443e4
FebriPratama <febri.pr@students.amikom.ac.id>
2017-04-02 09:45:18 +0700

    google

--- grabgan.php
+++ grabgan.php
@@ -621,6 +621,58 @@ Menu setting
 
     }
 
+	private static function doMagicGoogle($q){
+
+	    $curl = curl_init();
+
+	    $header[0] = "Accept: text/xml,application/xml,application/xhtml+xml,";
+	    $header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
+	    $header[] = "Cache-Control: max-age=0";
+	    $header[] = "Connection: keep-alive";
+	    $header[] = "Keep-Alive: 300";
+	    $header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
+	    $header[] = "Accept-Language: en-us,en;q=0.5";
+	    $header[] = "Pragma: ";
+
+	    $url = 'https://www.google.com/search?q='.urlencode($q).'&source=lnms&tbm=isch';
+
+	    curl_setopt($curl, CURLOPT_URL, $url);
+	    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:7.0.1) Gecko/20100101 Firefox/7.0.12011-10-16 20:23:00");
+	    curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+	    curl_setopt($curl, CURLOPT_REFERER, "https://www.facebook.com");
+	    curl_setopt($curl, CURLOPT_ENCODING, "gzip,deflate");
+	    curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+	    curl_setopt($curl, CURLOPT_FOLLOWLOCATION,true);
+
+	    $html = curl_exec($curl);
+	    //echo 'Curl error: '. curl_error($curl);
+	    curl_close($curl);
+
+		$hub = [];
+
+		$dom = NEW DOMDOcument();
+		@$dom->loadHTML($html);
+
+		$link = $dom->getElementsByTagName('div');
+
+		foreach($link as $a) {
+
+			if($a->getAttribute('class') == 'rg_meta') {
+
+				$string = json_decode($a->textContent);
+				
+				//Log::info($string->murl);
+				$hub[] = ['url' => $string->ou, 'title' => $string->s, 'height' => $string->oh, 'width' => $string->ow];
+
+			}
+		}
+
+		return array('status' => 1, 'data' => $hub,'message' => 'oce','total' => count($hub));
+
+	}
+
     public function getCat($id){
 
 		$args = array(
@@ -796,7 +848,7 @@ Menu setting
     	self::generateTag($kw,$post_id);
 
     	//fetch bing api
-		$results = self::bingSearch($kw.' ideas');
+		$results = self::doMagicGoogle($kw.' ideas');
 
 		require_once( ABSPATH . 'wp-admin/includes/image.php' );
 
@@ -804,15 +856,15 @@ Menu setting
 
 		$max= get_option('grabgan.total_post') ? (int)get_option('grabgan.total_post') : 15;
 
-		if(is_null($results)){
+		if(count($results['data']) < 1){
 			
 			return 2;
 
 		}
 
-		foreach ($results->d->results as $id => $value){
+		foreach ($results['data'] => $value){
 
-			$url = $value->MediaUrl;
+			$url = $value['url'];
 
 			$uploads = wp_upload_dir();
 			
